e91b7a26cc82fc384ba27bf1f050311f0616ce39
Camila Garcia Petiet <camigpetiet@gmail.com>
2026-05-13 12:55:26 -0300

    UI changes and hardcoded login form

--- src/components/LoginCard.tsx
+++ src/components/LoginCard.tsx
@@ -21,6 +21,21 @@ export function LoginCard() {
     setPassword(e.target.value);
   };
 
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+
+    if (!mail || !password) {
+      alert("Complete all fields");
+      return;
+    }
+
+    if (mail === "test@gmail.com" && password === "Test1234") {
+      navigate("/home");
+    } else {
+      alert("Wrong credentials");
+    }
+  };
+
   const navigate = useNavigate();
 
   return (
@@ -29,34 +44,30 @@ export function LoginCard() {
         <img src={login}></img>
       </div>
       <div className="login-header">
-        <h1 className="login-title">Iniciar Sesión</h1>
+        <h1 className="login-title">Welcome</h1>
         <h2 className="login-subtitle">
-          Ingresa tus credenciales para acceder al sistema
+          Enter your credentials to access the system
         </h2>
       </div>
-      <form className="login-form">
+      <form className="login-form" onSubmit={handleSubmit}>
         <Input
-          label="Correo electronico"
+          label="Email"
           placeholder="example@mail.com"
           type="mail"
           onChange={handleMailInput}
         />
         <Input
-          label="Contraseña"
+          label="Password"
           placeholder="*********"
           type="password"
           onChange={handlePasswordInput}
         />
-        <button
-          className="login-btn"
-          type="submit"
-          onClick={() => navigate("/home")}
-        >
-          Ingresar
+        <button className="login-btn" type="submit">
+          Sign in
         </button>
       </form>
 
-      <p className="newaccount-btn">Crear cuenta nueva</p>
+      <p className="newaccount-btn">Create new account</p>
     </div>
   );
 }
