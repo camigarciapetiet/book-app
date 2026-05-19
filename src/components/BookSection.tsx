d46e51272b1101e952be712764458b693924fb0a
Camila Garcia Petiet <camigpetiet@gmail.com>
2026-05-19 15:43:57 -0300

    add scroll functionality for book list

--- src/components/BookSection.tsx
+++ src/components/BookSection.tsx
@@ -1,6 +1,7 @@
 import "./BookSection.css";
 import { BookItem } from "../components/BookItem";
 import { Book } from "../pages/Home";
+import { useRef } from "react";
 
 type Props = {
   title: String;
@@ -9,18 +10,46 @@ type Props = {
 };
 
 export function BookSection({ title, books, onSelectBook }: Props) {
+  const containerRef = useRef<HTMLDivElement | null>(null);
+
+  const scrollLeft = () => {
+    if (containerRef.current) {
+      containerRef.current.scrollBy({
+        left: -300,
+        behavior: "smooth",
+      });
+    }
+  };
+
+  const scrollRight = () => {
+    if (containerRef.current) {
+      containerRef.current.scrollBy({
+        left: 300,
+        behavior: "smooth",
+      });
+    }
+  };
+
   return (
     <>
       <div className="book-section">
         <p className="section-title">{title}</p>
-        <div className="books-container">
-          {books?.map((book) => (
-            <BookItem
-              name={book.name}
-              image={book.image}
-              onClick={() => onSelectBook(book)}
-            />
-          ))}
+        <div className="books-wrapper">
+          <button className="scroll-btn left" onClick={scrollLeft}>
+            ◀
+          </button>
+          <div className="books-container" ref={containerRef}>
+            {books?.map((book) => (
+              <BookItem
+                name={book.name}
+                image={book.image}
+                onClick={() => onSelectBook(book)}
+              />
+            ))}
+          </div>
+          <button className="scroll-btn right" onClick={scrollRight}>
+            ▶
+          </button>
         </div>
       </div>
     </>
